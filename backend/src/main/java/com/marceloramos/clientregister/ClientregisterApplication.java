a229dc55cd96a09b56e704bfc32072ba295e7dac
Marcelo Ramos <marcelosr1973@gmail.com>
2021-05-26 20:06:40 -0300

    Project created

---
+++ backend/src/main/java/com/marceloramos/clientregister/ClientregisterApplication.java
@@ -0,0 +1,13 @@
+package com.marceloramos.clientregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientregisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientregisterApplication.class, args);
+	}
+
+}
